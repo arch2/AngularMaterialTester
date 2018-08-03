f70934dd1d6705951f5bdf25dc3d2cf27583fe1b
Matt Stratton <mattstrattn@gmail.com>
2018-08-03 16:45:34 -0400

    bleh changes

--- src/app/question.form.component.ts
+++ src/app/question.form.component.ts
@@ -3,18 +3,7 @@ import { FORM_DATA } from './form.data';
 
 @Component({
   selector: 'question-form',
-  template: `
-    <fieldset>
-      <div *ngFor="let question of questions; let i = index">
-        <div *ngIf="question.visible" [ngSwitch]="question.controlType" class="switch" >
-          <f-checkbox-group *ngSwitchCase="'checkboxGroup'" [dataProvider]="question.list"></f-checkbox-group>
-          <f-date-time-range *ngSwitchCase="'timeRange'" [label]="question.label" [start]="question.start" [end]="question.end"></f-date-time-range>
-          <f-dropdown *ngSwitchCase="'dropdown'" [dataProvider]="question.options" [label]="question.label"></f-dropdown>
-          <f-textbox *ngSwitchCase="'textbox'" [dataProvider]="question.key" [label]="question.label"></f-textbox>
-        </div>
-      </div>
-    </fieldset>
-  `
+  templateUrl: `question.form.component.html`
 })
 export class QuestionFormComponent {
   public questions: Array<any> = FORM_DATA;
